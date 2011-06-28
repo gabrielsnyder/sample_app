e45f2c6454f8d124fd4d198bf6171325d0f540c9
Gabriel Snyder <gs@gabrielsnyder.com>
2011-06-28 00:36:04 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'pages#home'
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,11 +57,7 @@ SampleApp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => "welcome#index"
-
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
